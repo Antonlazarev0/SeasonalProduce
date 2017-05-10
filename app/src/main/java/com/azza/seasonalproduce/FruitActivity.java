48d656df99c929d3e7556dc8f1c62043eb17ca24
Anton Lazarev <azza_nz@hotmail.com>
2017-05-10 15:03:30 +1000

    Debugged database connection now connects correctly for fruit will be testing and updating code for vegetables next

--- app/src/main/java/com/azza/seasonalproduce/FruitActivity.java
+++ app/src/main/java/com/azza/seasonalproduce/FruitActivity.java
@@ -1,13 +1,18 @@
 package com.azza.seasonalproduce;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FruitActivity extends AppCompatActivity {
 
-    public static final String EXTRA_FRUIT = "fruitdNo";
+    public static final String EXTRA_FRUITNO = "fruitNo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,47 @@ public class FruitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fruit);
 
         //Get the fruit from the intent
-        int fruitNo = (Integer)getIntent().getExtras().get(EXTRA_FRUIT);
-        Fruit fruit = Fruit.fruits[fruitNo];
+        int fruitNo = (Integer)getIntent().getExtras().get(EXTRA_FRUITNO);
+
+        //Create cursor
+        try {
+            SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(this);
+            SQLiteDatabase db = seasonalDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("FRUIT",
+                                        new String[] {"NAME", "DESCRIPTION", "SEASON", "IMAGE_RESOURCE_ID"},
+                                        "_id = ?",
+                                        new String[] {Integer.toString(fruitNo)},
+                                        null, null, null);
+            if(cursor.moveToFirst()) {
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                String seasonText = cursor.getString(2);
+                int photoId = cursor.getInt(3);
+
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                TextView season = (TextView) findViewById(R.id.season);
+                season.setText(seasonText);
+
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+            cursor.close();
+            db.close();
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+}
+
+/*Fruit fruit = Fruit.fruits[fruitNo];
 
         //Populate the fruit image
         ImageView photo = (ImageView) findViewById(R.id.photo);
@@ -33,8 +77,4 @@ public class FruitActivity extends AppCompatActivity {
 
         //Populate the fruit description
         TextView description = (TextView) findViewById(R.id.description);
-        description.setText(fruit.getDescription());
-
-
-    }
-}
+        description.setText(fruit.getDescription());*/
--- app/src/main/java/com/azza/seasonalproduce/FruitCategoryActivity.java
+++ app/src/main/java/com/azza/seasonalproduce/FruitCategoryActivity.java
@@ -2,25 +2,63 @@ package com.azza.seasonalproduce;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class FruitCategoryActivity extends ListActivity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView listProduce = getListView();
-        ArrayAdapter<Fruit> listAdapter = new ArrayAdapter<Fruit>(this, android.R.layout.simple_list_item_1, Fruit.fruits);
-        listProduce.setAdapter(listAdapter);
+
+        try{
+            SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(this);
+            db = seasonalDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("FRUIT",
+                                new String[]{"_id", "NAME"},
+                                null, null, null, null, null);
+
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                                            android.R.layout.simple_list_item_1,
+                                            cursor,
+                                            new String[]{"NAME"},
+                                            new int[]{android.R.id.text1},
+                                            0);
+            listProduce.setAdapter(listAdapter);
+        }catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 
     @Override
     protected void onListItemClick(ListView listView, View itemView, int position, long id) {
         Intent intent = new Intent(FruitCategoryActivity.this, FruitActivity.class);
-        intent.putExtra(FruitActivity.EXTRA_FRUIT, (int) id);
+        intent.putExtra(FruitActivity.EXTRA_FRUITNO, (int) id);
         startActivity(intent);
     }
 }
+
+     /*  ArrayAdapter<Fruit> listAdapter = new ArrayAdapter<Fruit>(this, android.R.layout.simple_list_item_1, Fruit.fruits);
+        listProduce.setAdapter(listAdapter);*/
