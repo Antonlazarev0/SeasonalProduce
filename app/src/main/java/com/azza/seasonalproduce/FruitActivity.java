eb75442eee737da136193f2e191e314ce5bf525a
Anton Lazarev <azza_nz@hotmail.com>
2017-05-11 14:52:28 +1000

    added a favorite checkbox in fruit and vegetable activity

--- app/src/main/java/com/azza/seasonalproduce/FruitActivity.java
+++ app/src/main/java/com/azza/seasonalproduce/FruitActivity.java
@@ -1,11 +1,14 @@
 package com.azza.seasonalproduce;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,9 +28,9 @@ public class FruitActivity extends AppCompatActivity {
         //Create cursor
         try {
             SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(this);
-            SQLiteDatabase db = seasonalDatabaseHelper.getReadableDatabase();
+            SQLiteDatabase db = seasonalDatabaseHelper.getWritableDatabase();
             Cursor cursor = db.query("FRUIT",
-                                        new String[] {"NAME", "DESCRIPTION", "SEASON", "IMAGE_RESOURCE_ID"},
+                                        new String[] {"NAME", "DESCRIPTION", "SEASON", "IMAGE_RESOURCE_ID", "FAVORITE"},
                                         "_id = ?",
                                         new String[] {Integer.toString(fruitNo)},
                                         null, null, null);
@@ -36,6 +39,7 @@ public class FruitActivity extends AppCompatActivity {
                 String descriptionText = cursor.getString(1);
                 String seasonText = cursor.getString(2);
                 int photoId = cursor.getInt(3);
+                boolean isFavorite = (cursor.getInt(4) == 1);
 
                 TextView name = (TextView) findViewById(R.id.name);
                 name.setText(nameText);
@@ -49,6 +53,9 @@ public class FruitActivity extends AppCompatActivity {
                 ImageView photo = (ImageView) findViewById(R.id.photo);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+
+                CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
             cursor.close();
             db.close();
@@ -56,7 +63,23 @@ public class FruitActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
 
+    public void onFavoriteClicked(View view) {
+        int fruitNo = (int) getIntent().getExtras().get("fruitNo");
+        CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+        ContentValues fruitValues = new ContentValues();
+        fruitValues.put("FAVORITE", favorite.isChecked());
+        SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(FruitActivity.this);
+
+        try {
+            SQLiteDatabase db = seasonalDatabaseHelper.getWritableDatabase();
+            db.update("FRUIT", fruitValues, "_id = ?", new String[]{Integer.toString(fruitNo)});
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, " Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
 
