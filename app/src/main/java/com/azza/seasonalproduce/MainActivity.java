85d643d36eb580edf592f1d76ee814d955e2de9a
Anton Lazarev <azza_nz@hotmail.com>
2017-06-13 20:39:10 +1000

    deleted scroll view in favour of 2 list views which are scrollable

--- app/src/main/java/com/azza/seasonalproduce/MainActivity.java
+++ app/src/main/java/com/azza/seasonalproduce/MainActivity.java
@@ -1,13 +1,24 @@
 package com.azza.seasonalproduce;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +41,76 @@ public class MainActivity extends AppCompatActivity {
         //Add the listener to the list view
         ListView listView = (ListView) findViewById(R.id.produce_options);
         listView.setOnItemClickListener(itemClickListener);
+
+        //Populate the list_favorite_favorite_fruit ListView from a cursor
+        ListView listFavoriteFruit = (ListView) findViewById(R.id.list_favourite_fruit);
+        try {
+            SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(this);
+            db = seasonalDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("FRUIT",
+                    new String[] { "_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(MainActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1}, 0);
+            listFavoriteFruit.setAdapter(favoriteAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        //Navigate to the fruit activity if a fruit is clicked
+        listFavoriteFruit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, FruitActivity.class);
+                intent.putExtra(FruitActivity.EXTRA_FRUITNO, (int)id);
+                startActivity(intent);
+            }
+        });
+
+        //Populate the list_favorite_favorite_vegetable ListView from a cursor
+        ListView listFavoriteVegetable = (ListView) findViewById(R.id.list_favourite_vegetable);
+        try {
+            SQLiteOpenHelper seasonalDatabaseHelper = new SeasonalDatabaseHelper(this);
+            db = seasonalDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("VEGETABLE",
+                    new String[] { "_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(MainActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1}, 0);
+            listFavoriteVegetable.setAdapter(favoriteAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        //Navigate to the vegetable activity if a vegetable is clicked
+        listFavoriteVegetable.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, VegetableActivity.class);
+                intent.putExtra(VegetableActivity.EXTRA_VEGENO, (int)id);
+                startActivity(intent);
+            }
+        });
     }
+
+    //Close the cursor and database in the onDestroy()
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
+
 }
